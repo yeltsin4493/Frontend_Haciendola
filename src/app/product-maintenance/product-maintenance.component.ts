046cca8598d264581f2e12c34136e4f199c0067b
Yeltsin <yeltsinrhp.93@gmail.com>
2024-05-10 08:56:12 -0500

    change advance project

--- src/app/product-maintenance/product-maintenance.component.ts
+++ src/app/product-maintenance/product-maintenance.component.ts
@@ -1,8 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BackendService } from '../services/backend.service';
+import { ToastrService } from 'ngx-toastr';
+
+// @ts-ignore
+const $: any = window['$'];
 
 @Component({
   selector: 'app-product-maintenance',
@@ -12,10 +16,26 @@ import { BackendService } from '../services/backend.service';
   styleUrl: './product-maintenance.component.css',
 })
 export class ProductMaintenanceComponent implements OnInit {
-  currentUser: any; // Usuario actual
+  @ViewChild('modal') modal?: ElementRef;
+  @ViewChild('modaledit') modaledit?: ElementRef;
+  @ViewChild('modaldelete') modaldelete?: ElementRef;
   products: any[] = []; // Lista de productos
+  selectedProduct: any = {};
+  newProduct: any = {
+    handle: '',
+    title: '',
+    description: '',
+    sku: '',
+    grams: '',
+    stock: 0,
+    price: '',
+    compare_price: '',
+    barcode: '',
+  };
+  deleteConfirmationVisible: boolean = false;
+  productToDelete: any;
 
-  constructor(private router: Router, private backendService: BackendService) {}
+  constructor(private router: Router, private backendService: BackendService, private toastr: ToastrService) {}
 
   ngOnInit(): void {
     // Llama a loadProducts() cuando se inicializa el componente
@@ -30,25 +50,104 @@ export class ProductMaintenanceComponent implements OnInit {
 
   openModal(): void {
     // Abrir el modal para agregar un nuevo producto
-    // $('#newProductModal').modal('show');
+    $(this.modal?.nativeElement).modal('show');
   }
 
   loadProducts(): void {
-    this.backendService.loadProducts().subscribe(
-      (response) => {
+    this.backendService.loadProducts().subscribe({
+      next: (response) => {
         this.products = response;
+        this.toastr.success('Se listaron los productos', 'Éxito');
       },
-      (error) => {
+      error: (error) => {
         console.error('Error al cargar los productos:', error);
-      }
-    );
+      },
+    });
   }
 
-  editProduct(product: any): void {
+  saveProduct(): void {
+    this.backendService.saveProduct(this.newProduct).subscribe({
+      next: (response) => {
+        console.log('Producto guardado:', response);
+        $(this.modal?.nativeElement).modal('hide');
+
+        // Aquí puedes agregar lógica adicional, como mostrar un mensaje de éxito o limpiar el formulario
+      },
+      error: (error) => {
+        console.error('Error al guardar producto:', error);
+        // Aquí puedes agregar lógica para manejar errores, como mostrar un mensaje de error al usuario
+      },
+    });
+  }
+
+  modalEdit(product: any): void {
     // Implementar lógica para editar un producto
+    this.selectedProduct = product;
+    $(this.modaledit?.nativeElement).modal('show');
   }
 
-  deleteProduct(product: any): void {
-    // Implementar lógica para eliminar un producto
+  editProduct() {
+    const updatedProduct = {
+      handle: this.selectedProduct.handle,
+      title: this.selectedProduct.title,
+      description: this.selectedProduct.description,
+      sku: this.selectedProduct.sku,
+      grams: this.selectedProduct.grams,
+      stock: this.selectedProduct.stock,
+      price: this.selectedProduct.price,
+      compare_price: this.selectedProduct.compare_price,
+      barcode: this.selectedProduct.barcode,
+    };
+    console.log(this.selectedProduct);
+    this.backendService
+      .editProduct(this.selectedProduct.id, updatedProduct)
+      .subscribe({
+        next: (response) => {
+          console.log('Producto actualizado:', response);
+          $(this.modaledit?.nativeElement).modal('hide');
+
+          // Aquí puedes agregar lógica adicional, como mostrar un mensaje de éxito o recargar la lista de productos
+        },
+        error: (error) => {
+          console.error('Error al actualizar producto:', error);
+          // Aquí puedes agregar lógica para manejar errores, como mostrar un mensaje de error al usuario
+        },
+      });
+  }
+
+  modalDelete(product: any): void {
+    // Implementar lógica para editar un producto
+    this.productToDelete = product;
+    $(this.modaldelete?.nativeElement).modal('show');
+  }
+
+  showDeleteConfirmation(product: any): void {
+    $(this.modaldelete?.nativeElement).modal('show');
+    this.productToDelete = product;
+    this.deleteConfirmationVisible = true;
+  }
+
+  confirmDelete(): void {
+    if (this.productToDelete) {
+      this.backendService.deleteProduct(this.productToDelete.id).subscribe({
+        next: (response) => {
+          console.log('Producto eliminado:', this.productToDelete);
+          // Actualizar la lista de productos después de eliminar
+          this.loadProducts();
+          // Ocultar el modal de confirmación
+          this.hideDeleteConfirmation();
+        },
+        error: (error) => {
+          console.error('Error al eliminar producto:', error);
+          // Aquí puedes agregar lógica para manejar errores, como mostrar un mensaje de error al usuario
+        },
+      });
+    }
+  }
+
+  hideDeleteConfirmation(): void {
+    $(this.modaldelete?.nativeElement).modal('hide');
+    this.deleteConfirmationVisible = false;
+    this.productToDelete = null;
   }
 }
--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -37,4 +37,51 @@ export class BackendService {
     // Realizar la solicitud GET con el encabezado configurado
     return this.http.get<any[]>(`${this.baseUrl}/products`, { headers });
   }
+
+  saveProduct(productData: any): Observable<any> {
+    const token = localStorage.getItem('token');
+
+    // Verificar si se encontró un token en el localStorage
+    if (!token) {
+      throw new Error(
+        'No se encontró un token de autorización en el almacenamiento local.'
+      );
+    }
+
+    // Configurar el encabezado con el token de autorización
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    return this.http.post(`${this.baseUrl}/products`, productData, { headers });
+  }
+
+  editProduct(productId: string, updatedProduct: any): Observable<any> {
+    const url = `${this.baseUrl}/products/${productId}`;
+    // Asegúrate de tener un token de autorización en tu localStorage
+    const token = localStorage.getItem('token');
+    if (!token) {
+      throw new Error('Token de autorización no encontrado.');
+    }
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.patch(url, updatedProduct, { headers });
+  }
+
+  deleteProduct(productId: string): Observable<any> {
+    const url = `${this.baseUrl}/products/${productId}`;
+    // Asegúrate de tener un token de autorización en tu localStorage
+    const token = localStorage.getItem('token');
+    if (!token) {
+      throw new Error('Token de autorización no encontrado.');
+    }
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.delete(url, { headers });
+  }
 }
